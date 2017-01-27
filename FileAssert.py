aee8f633148f32e6790aabe713bea0e1cb3d38d2
beebrain <mynameisbee@gmail.com>
2017-01-27 11:27:54 +0700

    Start CNN Finger

--- FileAssert.py
+++ FileAssert.py
@@ -1,14 +1,20 @@
 import cv2
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 import time
 import math
 import sys
 from os import walk
+import gc
+import os
+import MainCut as MC
 
 class FileAssert(object):
     def __init__(self,currentPath = "image"):
         self.currentPath = currentPath
         self.configFile = "Dataset.txt"
+        self.imageData = "ImageDataset"
+        self.detailData = "DetailDataset"
         #self.writeEmptyDataset()
         #self.readFile(self.currentPath)
 
@@ -21,10 +27,94 @@ class FileAssert(object):
         text_file.write("%s \n" % text)
         text_file.close()
 
-    def readDataSetFile(self):
+    # for read Dataset.txt is config path of image
+    def readDataSetConfigFile(self):
         with open(self.configFile) as f:
             rawPath  = f.read().splitlines()
         return rawPath
+
+    #for read raw dataset , it's have (class,personal,filename,height,width,chanels,imagedata_on_one_dimetion)
+    #and unpackdataset to two value one is datadetail ,anather one is imagedataset
+    def unpackDataset(self):
+        if not (os.path.exists(self.detailData) and os.path.isfile(self.imageData)):
+            detailDataset = np.load(self.detailData+".npy")
+            imgDataset = np.load(self.imageData+".npy")
+
+            imgDataset = imgDataset.reshape((-1,200,200,3))
+
+            #Make sure dataset print out
+            # for index in xrange(len(img)):
+            #     cv2.imshow("x",img[index])
+            #     print detailDataset[index][1],detailDataset[index][0]
+            #     cv2.waitKey(0)
+            #     cv2.destroyAllWindows()
+            #
+
+            # change Personal to index personal
+            detailDataset[:,1] = [str.replace(w,"P","") for w in detailDataset[:,1]]
+            # print detailDataset
+
+        return (imgDataset.astype('uint8'),detailDataset[:,:2].astype('int'))
+
+    def resizeImage(self):
+        configpath = self.readDataSetConfigFile()
+        print configpath
+        for listpath in xrange(len(configpath)):
+            e = configpath[listpath]
+            list = str.split(e, ",")
+            image2 = cv2.imread(list[0])
+            image2 = cv2.resize(image2, (0, 0), fx=0.5, fy=0.5)
+            print list[0]
+            imageresize = MC.process(image2)
+            cv2.imwrite(str("./resize/" + list[3] + "_" + list[2] + "_" + str(listpath).zfill(4) + ".jpg"), imageresize)
+            # cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    def packData(self,path="./resize"):
+        if path == "":
+            print "Don't have File"
+        else:
+            imageData = []
+            detailData = []
+            for (dirpath, dirnames, filenames) in walk(path):
+                listFolder = []
+                listFile = []
+
+                listFile.extend(filenames)
+                listFolder.extend(dirnames)
+                for iFile in xrange(len(listFile)):
+                    filename = listFile[iFile]
+                    detailname = str.split(filename, "_")
+                    img = cv2.imread(path + "//" + filename)
+                    (h,w,c) = img.shape
+                    img = img.reshape(-1)
+                    newrow = np.array([detailname[0], detailname[1], filename])  # class,personal,filename,imageFile
+                    newrow = np.hstack((newrow,(h,w,c)))
+
+                    if(len(imageData) == 0):
+                        imageData = img
+                        detailData = newrow
+                    else:
+                        imageData = np.vstack((imageData,img))
+                        detailData = np.vstack((detailData, newrow))
+                    if(iFile%100 ==0):
+                        # gc.collect()
+                        print iFile
+
+
+            print "saving..."
+            if(len(imageData) == len(detailData)):
+                np.save(self.imageData, imageData)
+                np.save(self.detailData, detailData)
+            else:
+                print len(imageData)
+                print len(detailData)
+                print "Data Not Matched"
+            print "saved"
+        return imageData,detailData
+
+    def confirmDataset(self):
+        listdata = np.load("CompleteDataSet.npy")
 
     def writeDataSetFile(self,path = ""):
         if path == "":
@@ -41,4 +131,23 @@ class FileAssert(object):
                         print str(str.split(dirpath,"\\")[1:4]).strip('[]')+","+filenames[iFile]
                         print dirpath+"\\"+filenames[iFile]+","+str(str.split(dirpath,"\\")[1:4]).strip('[]').replace("'", "").replace(" ","")+","+filenames[iFile]
 
-# FileAssert().writeDataSetFile(".\Image")
+
+
+
+# imageDataset,detailDataset = FileAssert().packData()
+#
+# (x,y)=FileAssert().unpackDataset()
+# class_label = y[:,0]
+# personal = y[:,1]
+# print len(x)
+# rangeindex  = np.where(class_label==12)
+# class_label_selected = class_label[rangeindex]
+# print class_label_selected
+# # for idx, val in enumerate(rangeindex):
+# #     print val
+#     # print "aa",index
+#     # # cv2.imshow("xxx", x[index])
+#     # # cv2.waitKey(0)
+#     # # cv2.destroyAllWindows()
+#     # print class_label[index]
+#     # print personal[index]
