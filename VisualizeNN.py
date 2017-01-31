9bd93f0baca7ef740879fc0cf2a399b873befbe7
beebrain <mynameisbee@gmail.com>
2017-01-31 15:42:00 +0700

    Start CNN Finger

---
+++ VisualizeNN.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential,model_from_json
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.utils.visualize_util import plot
+from IPython.display import SVG
+from keras.utils.visualize_util import model_to_dot
+
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+
+plot(loaded_model, to_file='model.png')
+SVG(model_to_dot(loaded_model).create(prog='dot', format='svg'))
